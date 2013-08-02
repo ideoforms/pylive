b24409f5b0b5862448e5f5bf6af0e652b61b0b87
Daniel Jones <daniel@jones.org.uk>
2013-08-02 16:11:10 +0100

    added helper functions for common actions in the LiveOSC API

--- set.py
+++ set.py
@@ -1,25 +1,260 @@
-from live.query import *
-from live.group import *
-from live.track import *
+import live.query
+import live.group
+import live.track
 
 import re
 import sys
 
 import pickle
 
-@singleton
-class Set (LoggingObject):
+@live.singleton
+class Set (live.LoggingObject):
 	def __init__(self):
 		self.indent = 0
 		self.groups = []
 		self.tracks = []
 		self.group_re = re.compile("^(\d+)\. (\S.+)")
-		self.live = LiveQuery()
+		self.live = live.Query()
 		self.scanned = False
 
 	def __str__(self):
 		return "live.set"
+
+	#------------------------------------------------------------------------
+	# /live/tempo
+	#------------------------------------------------------------------------
+
+	def get_tempo(self):
+		return self.live.query("/live/tempo")[0]
+
+	def set_tempo(self, value):
+		self.live.cmd("/live/tempo", value)
+
+	tempo = property(get_tempo, set_tempo)
 	
+	#------------------------------------------------------------------------
+	# /live/time
+	#------------------------------------------------------------------------
+
+	def get_time(self):
+		return self.live.query("/live/time")[0]
+
+	def set_time(self, value):
+		self.live.cmd("/live/time", value)
+
+	time = property(get_time, set_time)
+
+	#------------------------------------------------------------------------
+	# /live/overdub
+	# (uses /live/state to query)
+	#------------------------------------------------------------------------
+
+	def get_overdub(self):
+		value = self.live.query("/live/state")
+		return value[1]
+
+	def set_overdub(self, value):
+		self.live.cmd("/live/overdub", value)
+
+	overdub = property(get_overdub, set_overdub)
+
+	#------------------------------------------------------------------------
+	# /live/state
+	#------------------------------------------------------------------------
+
+	@property
+	def state(self):
+		return self.live.query("/live/state")
+
+	#------------------------------------------------------------------------
+	# /live/undo
+	# /live/redo
+	#------------------------------------------------------------------------
+
+	def undo(self):
+		self.live.cmd("/live/undo")
+
+	def redo(self):
+		self.live.cmd("/live/redo")
+
+	#------------------------------------------------------------------------
+	# /live/prev/cue
+	# /live/next/cue
+	#------------------------------------------------------------------------
+
+	def prev_cue(self):
+		self.live.cmd("/live/prev/cue")
+	def next_cue(self):
+		self.live.cmd("/live/next/cue")
+
+	#------------------------------------------------------------------------
+	# /live/play
+	# /live/play/continue
+	# /live/play/selection
+	#------------------------------------------------------------------------
+
+	def play(self, reset = False):
+		if reset:
+			# play from start
+			self.live.cmd("/live/play")
+		else:
+			# continue playing
+			self.live.cmd("/live/play/continue")
+
+	#------------------------------------------------------------------------
+	# /live/stop
+	#------------------------------------------------------------------------
+
+	def stop(self):
+		self.live.cmd("/live/stop")
+
+	#------------------------------------------------------------------------
+	# /live/scenes
+	#------------------------------------------------------------------------
+
+	@property
+	def num_scenes(self):
+		return self.live.query("/live/scenes")[0]
+
+	#------------------------------------------------------------------------
+	# /live/tracks
+	#------------------------------------------------------------------------
+
+	@property
+	def num_tracks(self):
+		return self.live.query("/live/tracks")[0]
+
+	#------------------------------------------------------------------------
+	# /live/scene
+	#------------------------------------------------------------------------
+
+	def get_scene(self):
+		return self.live.query("/live/scene")[0]
+
+	def set_scene(self, value):
+		self.live.cmd("/live/scene", value)
+
+	scene = property(get_scene, set_scene)
+
+	#------------------------------------------------------------------------
+	# /live/name/scene
+	#------------------------------------------------------------------------
+
+	@property
+	def scene_names(self):
+		rv = self.live.query("/live/name/scene")
+		rv = [ rv[a] for a in range(1, len(rv), 2) ]
+		return rv
+	get_scene_names = scene_names
+
+	def get_scene_name(self, index):
+		return self.live.query("/live/name/scene", index)
+
+	def set_scene_name(self, index, value):
+		self.live.cmd("/live/name/scene", index, value)
+
+	#------------------------------------------------------------------------
+	# /live/name/track
+	#------------------------------------------------------------------------
+
+	@property
+	def track_names(self):
+		rv = self.live.query("/live/name/track")
+		rv = [ rv[a] for a in range(1, len(rv), 2) ]
+		return rv
+	get_track_names = track_names
+
+	def get_track_name(self, index):
+		return self.live.query("/live/name/track", index)
+
+	def set_track_name(self, index, value):
+		self.live.cmd("/live/name/track", index, value)
+
+	#------------------------------------------------------------------------
+	# /live/arm
+	#------------------------------------------------------------------------
+
+	def get_track_arm(self, track):
+		return self.live.query("/live/arm", track)[1]
+
+	def set_track_arm(self, track, arm):
+		self.live.cmd("/live/arm", track, arm)
+
+	#------------------------------------------------------------------------
+	# /live/mute
+	#------------------------------------------------------------------------
+
+	def get_track_mute(self, track):
+		return self.live.query("/live/mute", track)[1]
+
+	def set_track_mute(self, track, mute):
+		self.live.cmd("/live/mute", track, mute)
+
+	#------------------------------------------------------------------------
+	# /live/solo
+	#------------------------------------------------------------------------
+
+	def get_track_solo(self, track):
+		return self.live.query("/live/solo", track)[1]
+
+	def set_track_solo(self, track, solo):
+		self.live.cmd("/live/solo", track, solo)
+
+	#------------------------------------------------------------------------
+	# /live/volume
+	#------------------------------------------------------------------------
+
+	def get_track_volume(self, track):
+		return self.live.query("/live/volume", track)[1]
+
+	def set_track_volume(self, track, volume):
+		print "setting %d to %.3f" % (track, volume)
+		self.live.cmd("/live/volume", track, volume)
+
+	#------------------------------------------------------------------------
+	# /live/pan
+	#------------------------------------------------------------------------
+
+	def get_track_pan(self, track):
+		return self.live.query("/live/pan", track)[1]
+
+	def set_track_pan(self, track, pan):
+		self.live.cmd("/live/pan", track, pan)
+
+	#------------------------------------------------------------------------
+	# /live/send
+	#------------------------------------------------------------------------
+
+	def get_track_send(self, track, send):
+		return self.live.query("/live/send", track, send)[2]
+
+	def set_track_send(self, track, send, value):
+		self.live.cmd("/live/send", track, send, value)
+
+	#------------------------------------------------------------------------
+	# /live/master/volume
+	# /live/master/pan
+	#------------------------------------------------------------------------
+
+	def get_master_volume(self):
+		return self.live.query("/live/master/volume")[0]
+
+	def set_master_volume(self, value):
+		self.live.cmd("/live/master/volume", value)
+
+	master_volume = property(get_master_volume, set_master_volume)
+
+	def get_master_pan(self):
+		return self.live.query("/live/master/pan")[0]
+
+	def set_master_pan(self, value):
+		self.live.cmd("/live/master/pan", value)
+
+	master_pan = property(get_master_pan, set_master_pan)
+
+	#------------------------------------------------------------------------
+	#------------------------------------------------------------------------
+
 	def scan(self):
 		tracks = self.live.query("/live/tracks")
 		if not tracks:
@@ -54,14 +289,14 @@ class Set (LoggingObject):
 			if match:
 				group_index = int(match.group(1))
 				group_name = match.group(2)
-				group = Group(track_index, group_index, group_name)
+				group = live.Group(track_index, group_index, group_name)
 				current_group = group
 				self.groups.append(group)
 			else:
 				# TODO: consistence between Group and Track constructors
 				track_info = self.live.query("/live/track/info", track_index)
 				clips = track_info[2:]
-				track = Track(track_name, track_info, current_group)
+				track = live.Track(track_name, track_info, current_group)
 				if current_group is not None:
 					current_group.add_track(track)
 				self.tracks.append(track)
