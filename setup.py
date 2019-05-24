f33f163bc445b87406104de6555fb325a5bc4526
Daniel Jones <daniel@jones.org.uk>
2019-05-24 23:58:12 +0100

    Tabs -> spaces

--- setup.py
+++ setup.py
@@ -6,27 +6,28 @@ from setuptools import setup
 # Generate a PyPI-friendly RST file from our markdown README.
 #------------------------------------------------------------------------
 try:
-	import pypandoc
-	long_description = pypandoc.convert('README.md', 'rst')
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
 except:
-	long_description = None
+    long_description = None
 
 setup(
-	name = 'pylive',
-	version = '0.1.5',
-	description = 'Python remote control of Ableton Live',
-	long_description = long_description,
-	author = 'Daniel Jones',
-	author_email = 'dan-pylive@erase.net',
-	url = 'https://github.com/ideoforms/pylive',
-	packages = ['live'],
-	install_requires = ['pyliblo >= 0.9.1'],
-	keywords = ('sound', 'music', 'ableton', 'osc'),
-	classifiers = [
-		'Topic :: Multimedia :: Sound/Audio',
-		'Topic :: Artistic Software',
-		'Topic :: Communications',
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers'
-	]
+    name = 'pylive',
+    version = '0.1.5',
+    description = 'Python remote control of Ableton Live',
+    long_description = long_description,
+    author = 'Daniel Jones',
+    author_email = 'dan-pylive@erase.net',
+    url = 'https://github.com/ideoforms/pylive',
+    packages = ['live'],
+    install_requires = ['pyliblo >= 0.9.1'],
+    keywords = ('sound', 'music', 'ableton', 'osc'),
+    classifiers = [
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Artistic Software',
+        'Topic :: Communications',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers'
+    ],
+    test_suite='tests',
 )
