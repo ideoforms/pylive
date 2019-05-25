d6b74665d4c7bca03d3a65e88754d662148b9d9b
Daniel Jones <daniel@jones.org.uk>
2019-05-25 17:35:41 +0100

    Add __init__.py to tests

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+#------------------------------------------------------------------------
+# This file is included so that pytest can find the package
+# root and import the `live` module from a local relative path.
+#
+# https://docs.pytest.org/en/latest/goodpractices.html#test-package-name
+#------------------------------------------------------------------------
