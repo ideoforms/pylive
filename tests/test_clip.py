cfd2e9d3ac9d4391919b1b30d77f20323224a1b7
Daniel Jones <daniel@jones.org.uk>
2019-05-27 20:33:51 +0100

    Add tests for Clip stop, pitch

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -13,19 +13,38 @@ def setup_module():
 def live_set():
     set = live.Set()
     set.scan(scan_devices=True, scan_clip_names=True)
+    set.quantization = 0
+    set.tracks[4].stop()
+    time.sleep(0.2)
     return set
 
 @pytest.fixture(scope="module")
 def clip(live_set):
-    track = live_set.tracks[1]
+    track = live_set.tracks[4]
     clip = track.clips[0]
     return clip
 
 def test_clip_properties(clip, live_set):
-    assert clip.track == live_set.tracks[1]
+    assert clip.track == live_set.tracks[4]
     assert clip.set == live_set
     assert clip.index == 0
     assert clip.length == 4
     assert clip.looplen == 4
     assert clip.state == live.CLIP_STATUS_STOPPED
     assert clip.name == "one"
+
+def test_clip_play_stop(clip):
+    clip.play()
+    time.sleep(0.2)
+    assert clip.state == live.CLIP_STATUS_PLAYING
+    clip.stop()
+    time.sleep(0.2)
+    assert clip.state == live.CLIP_STATUS_STOPPED
+
+def test_clip_pitch(clip):
+    pitch = clip.pitch
+    assert tuple(pitch) == (0, 0)
+
+    clip.pitch = [ 1.0, 1.0 ]
+    pitch = clip.pitch
+    assert tuple(pitch) == (1, 1)
