f5ad6d073d29b9d800e264f310c5b4e2582108ab
Daniel Jones <daniel@jones.org.uk>
2019-05-27 20:33:21 +0100

    Use Track fixture in unit etsts

--- tests/test_track.py
+++ tests/test_track.py
@@ -10,36 +10,33 @@ def setup_module():
     open_test_set()
 
 @pytest.fixture(scope="module")
-def live_set():
+def track():
     set = live.Set()
     set.scan(scan_devices = True)
-    return set
+    set.tracks[1].stop()
+    time.sleep(0.1)
+    return set.tracks[1]
 
-def test_track_get_clips(live_set):
-    track = live_set.tracks[1]
+def test_track_get_clips(track):
     assert len(track.clips) == 5
 
-def test_track_get_active_clips(live_set):
-    track = live_set.tracks[1]
+def test_track_get_active_clips(track):
     assert len(track.active_clips) == 4
 
-def test_track_get_devices(live_set):
-    track = live_set.tracks[1]
+def test_track_get_devices(track):
     assert len(track.devices) == 1
 
-def test_track_scene_indexes(live_set):
-    track = live_set.tracks[1]
+def test_track_scene_indexes(track):
     scene_indexes = track.scene_indexes
     assert scene_indexes == [ 0, 1, 2, 4 ]
 
-def test_track_states(live_set):
+def test_track_states(track):
     # is_stopped
     # is_starting
     # is_playing
-    track = live_set.tracks[1]
-    live_set.quantization = 5
+    track.set.quantization = 5
     assert track.is_stopped
-    live_set.play()
+    track.set.play()
     time.sleep(0.1)
     track.clips[0].play()
     time.sleep(0.2)
@@ -49,10 +46,11 @@ def test_track_states(live_set):
     track.stop()
     time.sleep(1.0)
     assert track.is_stopped
+    track.set.stop()
 
-def test_track_stop(live_set):
-    live_set.quantization = 0
-    track = live_set.tracks[1]
+def test_track_stop(track):
+    track.set.quantization = 0
+    time.sleep(0.1)
     track.clips[0].play()
     time.sleep(0.2)
     assert track.is_playing
@@ -60,8 +58,7 @@ def test_track_stop(live_set):
     time.sleep(0.2)
     assert track.is_stopped
 
-def test_track_scan_clip_names(live_set):
-    track = live_set.tracks[1]
+def test_track_scan_clip_names(track):
     assert track.clips[0].name is None
     track.scan_clip_names()
     assert track.active_clips[0].name == "one"
@@ -69,50 +66,43 @@ def test_track_scan_clip_names(live_set):
     assert track.active_clips[2].name == "three"
     assert track.active_clips[3].name == "four"
 
-def test_track_volume(live_set):
-    track = live_set.tracks[2]
+def test_track_volume(track):
     assert track.volume == pytest.approx(0.85)
     track.volume = 0.0
     assert track.volume == 0.0
     track.volume = 0.85
 
-def test_track_pan(live_set):
-    track = live_set.tracks[2]
+def test_track_pan(track):
     assert track.pan == 0
     track.pan = 1
     assert track.pan == 1
     track.pan = 0
 
-def test_track_mute(live_set):
-    track = live_set.tracks[2]
+def test_track_mute(track):
     assert track.mute == 0
     track.mute = 1
     assert track.mute == 1
     track.mute = 0
 
-def test_track_arm(live_set):
-    track = live_set.tracks[2]
+def test_track_arm(track):
     assert track.arm == 0
     track.arm = 1
     assert track.arm == 1
     track.arm = 0
 
-def test_track_solo(live_set):
-    track = live_set.tracks[2]
+def test_track_solo(track):
     assert track.solo == 0
     track.solo = 1
     assert track.solo == 1
     track.solo = 0
 
-def test_track_send(live_set):
-    track = live_set.tracks[2]
+def test_track_send(track):
     assert track.get_send(0) == pytest.approx(0.0)
     track.set_send(0, 1.0)
     assert track.get_send(0) == 1.0
     track.set_send(0, 0.0)
 
-def test_track_device_named(live_set):
-    track = live_set.tracks[2]
+def test_track_device_named(track):
     device = track.get_device_named("Operator")
     assert device is not None
     assert device == track.devices[0]
