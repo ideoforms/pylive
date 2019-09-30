d72341d9aa6b9d93039873a22cc5c4e1d9a9bbae
Daniel Jones <daniel@jones.org.uk>
2019-09-30 15:21:59 +0100

    Update version to 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'pylive',
-    version = '0.2.0',
+    version = '0.2.1',
     description = 'Python remote control of Ableton Live',
     long_description = open("README.md", "r").read(),
     long_description_content_type = "text/markdown",
